31127710537af00bec00c9b3f7373e3f32739311
vagrant <vagrant@rails-dev-box>
2014-12-16 07:52:35 +0000

    Sorting out ActionMailer error

--- config/initializers/mail.rb
+++ config/initializers/mail.rb
@@ -1,16 +1,16 @@
-ActionMailer::Base.smtp_settings = {
-  port:              587, 
-  address:           'smtp.mailgun.org',
-  user_name:         ENV['MAILGUN_SMTP_LOGIN'],
-  password:          ENV['MAILGUN_SMTP_PASSWORD'],
-  domain:            'herokuapp.com',
-  authentication:    :plain,
-  content_type:      'text/html'
-}
-ActionMailer::Base.delivery_method = :smtp
+# ActionMailer::Base.smtp_settings = {
+#   port:              587, 
+#   address:           'smtp.mailgun.org',
+#   user_name:         ENV['MAILGUN_SMTP_LOGIN'],
+#   password:          ENV['MAILGUN_SMTP_PASSWORD'],
+#   domain:            'herokuapp.com',
+#   authentication:    :plain,
+#   content_type:      'text/html'
+# }
+# ActionMailer::Base.delivery_method = :smtp
 
-# Makes debugging *way* easier.
-ActionMailer::Base.raise_delivery_errors = true
+# # Makes debugging *way* easier.
+# ActionMailer::Base.raise_delivery_errors = true
 
 # # This interceptor just makes sure that local mail 
 # # only emails you.
